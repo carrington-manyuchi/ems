82c20df001223a48d0dc236c0c6b2e71c19afb80
Carrington <Manyuchi>
2023-11-04 18:02:34 +0200

    create employee

--- da-server/index.js
+++ da-server/index.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
 const cors = require("cors");
+const EmployeeModel = require("./models/Employees");
+
 const app = express();
 
 app.use(cors());
-app.use(express.json);
+app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/da_employees");
+
+app.post("/createEmployee", (req, res) => {
+  EmployeeModel.create(req.body).then((employees) =>
+    res.json(employees).catch((err) => res.json(err))
+  );
+});
+
+app.listen(3001, () => {
+  console.log("Server is Running");
+});
---
+++ da-server/models/Employees.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const EmployeeSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  department: String,
+  status: String,
+});
+
+const EmployeeModel = mongoose.model("employees", EmployeeSchema);
+
+module.exports = EmployeeModel;
